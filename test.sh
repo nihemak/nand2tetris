6634dbdcf71d5c08fcc67e78648abf676f4d2a5c
Makoto Nihei <nihemak@gmail.com>
2019-04-08 08:26:22 +0900

    add CHIP Xor

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 ./nand2tetris/tools/HardwareSimulator.sh 01/Not.tst
 ./nand2tetris/tools/HardwareSimulator.sh 01/And.tst
 ./nand2tetris/tools/HardwareSimulator.sh 01/Or.tst
+./nand2tetris/tools/HardwareSimulator.sh 01/Xor.tst
