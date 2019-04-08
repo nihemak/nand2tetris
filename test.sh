5e37565582cd5fcc87353944966e0dd2a80f6bf4
Makoto Nihei <nihemak@gmail.com>
2019-04-09 08:02:31 +0900

    add CHIP Mux

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@
 ./nand2tetris/tools/HardwareSimulator.sh 01/And.tst
 ./nand2tetris/tools/HardwareSimulator.sh 01/Or.tst
 ./nand2tetris/tools/HardwareSimulator.sh 01/Xor.tst
+./nand2tetris/tools/HardwareSimulator.sh 01/Mux.tst
