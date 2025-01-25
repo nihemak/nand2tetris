819bb079835d0b1fa15b292b165d7544c6765aeb
nihemak <nihemak@gmail.com>
2025-01-25 15:38:45 +0900

    Add prefix underscore to unused variables

--- 13/HardwareSimulatorWasmBuiltIn/build.rs
+++ 13/HardwareSimulatorWasmBuiltIn/build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     // FIXME: Remove this
-    if let Ok(val) = env::var("TEST") {
+    if let Ok(_val) = env::var("TEST") {
     } else {
         // https://doc.rust-jp.rs/rust-by-example-ja/cargo/build_scripts.html
         // https://github.com/rustwasm/wasm-bindgen/issues/3368#issuecomment-1483954797
