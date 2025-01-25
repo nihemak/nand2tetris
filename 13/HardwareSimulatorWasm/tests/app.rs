bb696d1f66c1d392f2de929cd115b5ba3bfd18f7
nihemak <nihemak@gmail.com>
2025-01-25 15:25:32 +0900

    Remove unused imports

--- 13/HardwareSimulatorWasm/tests/app.rs
+++ 13/HardwareSimulatorWasm/tests/app.rs
@@ -1,6 +1,3 @@
-use futures::prelude::*;
-use wasm_bindgen::JsValue;
-use wasm_bindgen_futures::JsFuture;
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
