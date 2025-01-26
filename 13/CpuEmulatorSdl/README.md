34c80b29793ee93c0116b2a7fa89a944ebb7eb46
nihemak <nihemak@gmail.com>
2025-01-26 18:11:35 +0900

    Add CpuEmulator

---
+++ 13/CpuEmulatorSdl/README.md
@@ -0,0 +1,13 @@
+# CpuEmulator (SDL)
+
+```bash
+# test all
+cargo test
+
+# execute
+cargo run
+
+# execute (release build)
+cargo build --release
+./target/release/cpu_emulator_sdl
+```
---
+++ 13/CpuEmulatorWasm/src/browser.rs
@@ -0,0 +1,77 @@
+use anyhow::{anyhow, Result};
+use std::future::Future;
+use wasm_bindgen::{
+    closure::WasmClosure, prelude::Closure, JsCast,
+};
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement, Window,
+};
+
+// Straight taken from https://rustwasm.github.io/book/game-of-life/debugging.html
+#[allow(unused_macros)]
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+pub fn window() -> Result<Window> {
+    web_sys::window().ok_or_else(|| anyhow!("No Window Found"))
+}
+
+pub fn document() -> Result<Document> {
+    window()?
+        .document()
+        .ok_or_else(|| anyhow!("No Document Found"))
+}
+
+pub fn canvas() -> Result<HtmlCanvasElement> {
+    document()?
+        .get_element_by_id("canvas")
+        .ok_or_else(|| anyhow!("No Canvas Element found with ID 'canvas'"))?
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|element| anyhow!("Error converting {:#?} to HtmlCanvasElement", element))
+}
+
+pub fn context() -> Result<CanvasRenderingContext2d> {
+    canvas()?
+        .get_context("2d")
+        .map_err(|js_value| anyhow!("Error getting 2d context {:#?}", js_value))?
+        .ok_or_else(|| anyhow!("No 2d context found"))?
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .map_err(|element| {
+            anyhow!(
+                "Error converting {:#?} to CanvasRenderingContext2d",
+                element
+            )
+        })
+}
+
+pub fn spawn_local<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future);
+}
+
+pub type LoopClosure = Closure<dyn FnMut(f64)>;
+pub fn create_raf_closure(f: impl FnMut(f64) + 'static) -> LoopClosure {
+    closure_wrap(Box::new(f))
+}
+
+pub fn request_animation_frame(callback: &LoopClosure) -> Result<i32> {
+    window()?
+        .request_animation_frame(callback.as_ref().unchecked_ref())
+        .map_err(|err| anyhow!("Cannot request animation frame {:#?}", err))
+}
+
+pub fn closure_wrap<T: WasmClosure + ?Sized>(data: Box<T>) -> Closure<T> {
+    Closure::wrap(data)
+}
+
+pub fn now() -> Result<f64> {
+    Ok(window()?
+        .performance()
+        .ok_or_else(|| anyhow!("Performance object not found"))?
+        .now())
+}
